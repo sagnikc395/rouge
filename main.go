164fcfe826b45b555614b452e244eeb36b74ee1b
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-07-26 23:35:11 +0530

    src: render a black box ui

--- main.go
+++ main.go
@@ -1,56 +1,41 @@
 package main
 
 import (
-	_ "image/png"
-	"log"
-
 	"github.com/hajimehoshi/ebiten/v2"
+	"log"
 )
 
-// Game struct willl hold all the data the entire game will ned.
-type Game struct {
-	Map GameMap
-}
+//game holds all the data the entire game will need
 
-//create a new game object and initialize the data
+type Game struct{}
+
+//constructor for the new game
 
 func NewGame() *Game {
 	g := &Game{}
-	g.Map = NewGameMap()
 	return g
 }
 
-// callbacks for update, draw and layout
+// update the game on each of the tic
 func (g *Game) Update() error {
 	return nil
 }
 
-// draw similarly is called draw cycle and is where we will blit
+// draw will be called on each draw cycle and is where we will blip
 func (g *Game) Draw(screen *ebiten.Image) {
-	gd := NewGameData()
-	level := g.Map.Dungeons[0].Levels[0]
-	for x := 0; x < gd.ScreenWidth; x++ {
-		for y := 0; y < gd.ScreenHeight; y++ {
-			tile := level.Tiles[level.GetIndexFromXY(x, y)]
-			op := &ebiten.DrawImageOptions{}
-			op.GeoM.Translate(float64(tile.PixelX), float64(tile.PixelY))
-			screen.DrawImage(tile.Image, op)
-		}
-	}
+
 }
 
-//layout will return the screen dimensions
-
+// layout will return the screen dimensions
 func (g *Game) Layout(w, h int) (int, int) {
-	gd := NewGameData()
-	return gd.TileWidth * gd.ScreenWidth, gd.TileHeight * gd.ScreenHeight
+	return 1280, 800
 }
 
+// main render loop
 func main() {
-	//main loop to render
 	g := NewGame()
 	ebiten.SetWindowResizable(true)
-	ebiten.SetWindowTitle("Rouge")
+	ebiten.SetWindowTitle("Tower Defense")
 	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
